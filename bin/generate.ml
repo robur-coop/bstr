a43e0b5a9c1bc728e408772ffe1cdd8548a3e314
Calascibetta Romain <romain.calascibetta@gmail.com>
2026-08-29 15:46:06 +0200

    Fix our generate.exe program and how we replace module name
    We just ensure that we replace only OCaml module name

--- bin/generate.ml
+++ bin/generate.ml
@@ -3,34 +3,33 @@ let new_module = ref "String"
 let input = ref None
 let output = ref None
 
-let splits ~sep str =
-  let sep_len = String.length sep in
-  if sep_len = 0 then invalid_arg "splits: empty separator not allowed";
-  let str_len = String.length str in
-  let max_sep_idx = sep_len - 1 in
-  let max_str_idx = str_len - sep_len in
-  let add_sub str ~start ~stop acc =
-    if start = stop then "" :: acc
-    else String.sub str start (stop - start) :: acc
-  in
-  let rec check_sep start i k acc =
-    if k > max_sep_idx then
-      let new_start = i + sep_len in
-      scan new_start new_start (add_sub str ~start ~stop:i acc)
-    else if str.[i + k] = sep.[k] then check_sep start i (k + 1) acc
-    else scan start (i + 1) acc
-  and scan start i acc =
-    if i > max_str_idx then
-      if start = 0 then [ str ]
-      else List.rev (add_sub str ~start ~stop:str_len acc)
-    else if str.[i] = sep.[0] then check_sep start i 1 acc
-    else scan start (i + 1) acc
-  in
-  scan 0 0 []
+let is_ident = function
+  | 'a' .. 'z' | 'A' .. 'Z' | '0' .. '9' | '_' | '\'' -> true
+  | _ -> false
 
 let replace line module_to_be_replaced new_module =
-  let line = splits ~sep:(module_to_be_replaced ^ ".") line in
-  String.concat (new_module ^ ".") line
+  let len = String.length line in
+  let mlen = String.length module_to_be_replaced in
+  let buf = Buffer.create len in
+  let matches idx =
+    idx + mlen < len
+    && (idx = 0 || is_ident line.[idx - 1] = false)
+    && line.[idx + mlen] = '.'
+    && String.sub line idx mlen = module_to_be_replaced
+  in
+  let rec go idx =
+    if idx >= len then Buffer.contents buf
+    else if matches idx then begin
+      Buffer.add_string buf new_module;
+      Buffer.add_char buf '.';
+      go (idx + mlen + 1)
+    end
+    else begin
+      Buffer.add_char buf line.[idx];
+      go (idx + 1)
+    end
+  in
+  go 0
 
 let run () =
   let ic, ic_finally =
