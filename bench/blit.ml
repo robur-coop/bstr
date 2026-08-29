2efbc4d14bb2501679252a60f9dc80940c68b8d0
Calascibetta Romain <romain.calascibetta@gmail.com>
2026-08-29 15:48:06 +0200

    Upgrade our benchmark with our Slice module

--- bench/blit.ml
+++ bench/blit.ml
@@ -5,18 +5,30 @@ let src = String.make 256 '\x10'
 let cs = Cstruct.create 512
 let bstr = Bstr.create 512
 let bigstringaf = Bigstringaf.create 512
+let slice_bytes = Slice_bytes.create 512
+let slice_bstr = Slice_bstr.create 512
 let cstruct_blit () = Cstruct.blit_from_string src 0 cs 0 256
 let bstr_blit () = Bstr.blit_from_string src ~src_off:0 bstr ~dst_off:0 ~len:256
 
 let bigstringaf_blit () =
   Bigstringaf.blit_from_string src ~src_off:0 bigstringaf ~dst_off:0 ~len:256
 
+let slice_bytes_blit () =
+  Slice_bytes.blit_from_string src ~src_off:0 slice_bytes ~dst_off:0 256
+
+let slice_bstr_blit () =
+  Slice_bstr.blit_from_string src ~src_off:0 slice_bstr ~dst_off:0 256
+
 let cstruct_blit = Staged.stage cstruct_blit
 let bstr_blit = Staged.stage bstr_blit
 let bigstringaf_blit = Staged.stage bigstringaf_blit
+let slice_bytes_blit = Staged.stage slice_bytes_blit
+let slice_bstr_blit = Staged.stage slice_bstr_blit
 let test0 = Test.make ~name:"Cstruct" cstruct_blit
 let test1 = Test.make ~name:"Bstr" bstr_blit
 let test2 = Test.make ~name:"Bigstringaf" bigstringaf_blit
+let test3 = Test.make ~name:"Slice_bytes" slice_bytes_blit
+let test4 = Test.make ~name:"Slice_bstr" slice_bstr_blit
 
 let benchmark () =
   let bootstrap = 0 and r_square = true and predictors = Measure.[| run |] in
@@ -27,9 +39,8 @@ let benchmark () =
   and quota = Time.second 1.0
   and kde = Some 1000 in
   let cfg = Benchmark.cfg ~limit ~stabilize ~quota ~kde () in
-  let tests =
-    Test.make_grouped ~name:"blit" ~fmt:"%s %s" [ test0; test1; test2 ]
-  in
+  let tests = [ test0; test1; test2; test3; test4 ] in
+  let tests = Test.make_grouped ~name:"blit" ~fmt:"%s %s" tests in
   let raw = Benchmark.all cfg instances tests in
   let res = List.map (fun i -> Analyze.all ols i raw) instances in
   let res = Analyze.merge ols instances res in
--- bench/sub.ml
+++ bench/sub.ml
@@ -3,19 +3,27 @@ open Toolkit
 
 let cs = Cstruct.create 32
 let bstr = Bstr.create 32
+let bytes = Bytes.create 32
 let slice : Slice_bstr.t = Slice_bstr.make bstr
+let slice_bytes = Slice_bytes.make bytes
 let cstruct_sub () = Cstruct.sub cs 8 8
 let bstr_sub () = Bstr.sub bstr ~off:8 ~len:8
 let bigstringaf_sub () = Bigstringaf.sub bstr ~off:8 ~len:8
 let slice_sub () = Slice.sub slice ~off:8 ~len:8
+let slice_bytes_sub () = Slice_bytes.sub slice_bytes ~off:8 ~len:8
+let slice_bstr_sub () = Slice_bstr.sub slice ~off:8 ~len:8
 let cstruct_sub = Staged.stage cstruct_sub
 let bstr_sub = Staged.stage bstr_sub
 let bigstringaf_sub = Staged.stage bigstringaf_sub
 let slice_sub = Staged.stage slice_sub
+let slice_bytes_sub = Staged.stage slice_bytes_sub
+let slice_bstr_sub = Staged.stage slice_bstr_sub
 let test0 = Test.make ~name:"Cstruct" cstruct_sub
 let test1 = Test.make ~name:"Bstr" bstr_sub
 let test2 = Test.make ~name:"Bigstringaf" bigstringaf_sub
 let test3 = Test.make ~name:"Slice" slice_sub
+let test4 = Test.make ~name:"Slice_bytes" slice_bytes_sub
+let test5 = Test.make ~name:"Slice_bstr" slice_bstr_sub
 
 let benchmark () =
   let bootstrap = 0 and r_square = true and predictors = Measure.[| run |] in
@@ -26,9 +34,8 @@ let benchmark () =
   and quota = Time.second 1.0
   and kde = Some 1000 in
   let cfg = Benchmark.cfg ~limit ~stabilize ~quota ~kde () in
-  let tests =
-    Test.make_grouped ~name:"sub" ~fmt:"%s %s" [ test0; test1; test2; test3 ]
-  in
+  let tests = [ test0; test1; test2; test3; test4; test5 ] in
+  let tests = Test.make_grouped ~name:"sub" ~fmt:"%s %s" tests in
   let raw = Benchmark.all cfg instances tests in
   let res = List.map (fun i -> Analyze.all ols i raw) instances in
   let res = Analyze.merge ols instances res in
