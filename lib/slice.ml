9e753961bcca20765f5dfea184613e975db55ab4
Calascibetta Romain <romain.calascibetta@gmail.com>
2026-08-29 15:47:39 +0200

    Finalize our slice library with what bstr/bytes can expose

--- lib/slice.ml
+++ lib/slice.ml
@@ -38,9 +38,11 @@ let shift t off =
   unsafe_sub t off len
 
 module type R = sig
+  type buf
   type t
 
-  val make : int -> char -> t
+  val create : int -> t
+  val make : ?off:int -> ?len:int -> buf -> t
   val init : int -> (int -> char) -> t
   val empty : t
   val length : t -> int
@@ -48,66 +50,24 @@ module type R = sig
   val chop : ?rev:bool -> t -> char option
   val hash : t -> int
   val equal : t -> t -> bool
+  val constant_equal : t -> t -> bool
   val compare : t -> t -> int
   val get : t -> int -> char
   val unsafe_get : t -> int -> char
-
   val get_int8 : t -> int -> int
-  (** [get_int8 bstr i] is [bstr]'s signed 8-bit integer starting at byte index
-      [i]. *)
-
   val get_uint8 : t -> int -> int
-  (** [get_uint8 bstr i] is [bstr]'s unsigned 8-bit integer starting at byte
-      index [i]. *)
-
   val get_uint16_ne : t -> int -> int
-  (** [get_int16_ne bstr i] is [bstr]'s native-endian unsigned 16-bit integer
-      starting at byte index [i]. *)
-
   val get_uint16_le : t -> int -> int
-  (** [get_int16_le bstr i] is [bstr]'s little-endian unsigned 16-bit integer
-      starting at byte index [i]. *)
-
   val get_uint16_be : t -> int -> int
-  (** [get_int16_be bstr i] is [bstr]'s big-endian unsigned 16-bit integer
-      starting at byte index [i]. *)
-
   val get_int16_ne : t -> int -> int
-  (** [get_int16_ne bstr i] is [bstr]'s native-endian signed 16-bit integer
-      starting at byte index [i]. *)
-
   val get_int16_le : t -> int -> int
-  (** [get_int16_le bstr i] is [bstr]'s little-endian signed 16-bit integer
-      starting at byte index [i]. *)
-
   val get_int16_be : t -> int -> int
-  (** [get_int16_be bstr i] is [bstr]'s big-endian signed 16-bit integer
-      starting at byte index [i]. *)
-
   val get_int32_ne : t -> int -> int32
-  (** [get_int32_ne bstr i] is [bstr]'s native-endian 32-bit integer starting at
-      byte index [i]. *)
-
   val get_int32_le : t -> int -> int32
-  (** [get_int32_le bstr i] is [bstr]'s little-endian 32-bit integer starting at
-      byte index [i]. *)
-
   val get_int32_be : t -> int -> int32
-  (** [get_int32_be bstr i] is [bstr]'s big-endian 32-bit integer starting at
-      byte index [i]. *)
-
   val get_int64_ne : t -> int -> int64
-  (** [get_int64_ne bstr i] is [bstr]'s native-endian 64-bit integer starting at
-      byte index [i]. *)
-
   val get_int64_le : t -> int -> int64
-  (** [get_int64_le bstr i] is [bstr]'s little-endian 64-bit integer starting at
-      byte index [i]. *)
-
   val get_int64_be : t -> int -> int64
-  (** [get_int64_be bstr i] is [bstr]'s big-endian 64-bit integer starting at
-      byte index [i]. *)
-
   val filter : (char -> bool) -> t -> t
   val filter_map : (char -> char option) -> t -> t
   val map : (char -> char) -> t -> t
@@ -120,23 +80,10 @@ module type R = sig
   val overlap : t -> t -> (int * int * int) option
   val append : t -> t -> t
   val starts_with : prefix:string -> t -> bool
-
   val is_prefix : affix:string -> t -> bool
-  (** [is_prefix ~affix bstr] is [true] iff [affix.[idx] = bstr.{idx}] for all
-      indices [idx] of [affix]. *)
-
   val ends_with : suffix:string -> t -> bool
-
   val is_suffix : affix:string -> t -> bool
-  (** [is_suffix ~affix bstr] is [true] iff [affix.[n - idx] = bstr.{m - idx}]
-      for all indices [idx] of [affix] with [n = String.length affix - 1] and
-      [m = length bstr - 1]. *)
-
   val is_infix : affix:string -> t -> bool
-  (** [is_infix ~affix bstr] is [true] iff there exists an index [j] in [bstr]
-      such that for all indices [i] of [affix] we have
-      [affix.[i] = bstr.{j + i}]. *)
-
   val for_all : (char -> bool) -> t -> bool
   val exists : (char -> bool) -> t -> bool
   val trim : ?drop:(char -> bool) -> t -> t
@@ -151,12 +98,24 @@ module type R = sig
   val split_on_char : char -> t -> t list
   val cut : ?rev:bool -> sep:string -> t -> (t * t) option
   val cuts : ?rev:bool -> ?empty:bool -> sep:string -> t -> t list
-  val index : t -> ?rev:bool -> ?from:int -> char -> int
-  val contains : t -> ?rev:bool -> ?from:int -> char -> bool
-  val concat : t -> t list -> t
+  val index : t -> ?off:int -> ?len:int -> char -> int option
+  val contains : t -> ?off:int -> ?len:int -> char -> bool
+  val concat : string -> t list -> t
+  val extend : t -> int -> int -> t
   val copy : t -> t
   val sub_string : t -> off:int -> len:int -> string
   val to_string : t -> string
+  val of_string : string -> t
+  val string : ?off:int -> ?len:int -> string -> t
+
+  val blit_to_bytes :
+    t -> ?src_off:int -> bytes -> dst_off:int -> len:int -> unit
+
+  val with_range : ?first:int -> ?len:int -> t -> t
+  val with_index_range : ?first:int -> ?last:int -> t -> t
+  val to_seq : t -> char Seq.t
+  val to_seqi : t -> (int * char) Seq.t
+  val of_seq : char Seq.t -> t
 end
 
 module type W = sig
@@ -178,6 +137,18 @@ module type W = sig
   val set_int64_ne : t -> int -> int64 -> unit
   val set_int64_le : t -> int -> int64 -> unit
   val set_int64_be : t -> int -> int64 -> unit
-  val fill : t -> off:int -> len:int -> char -> unit
-  val blit : t -> src_off:int -> t -> dst_off:int -> len:int -> unit
+  val fill : t -> ?off:int -> ?len:int -> char -> unit
+  val blit : t -> t -> unit
+  val blit_from_bytes : bytes -> src_off:int -> t -> ?dst_off:int -> int -> unit
+
+  val blit_from_string :
+    string -> src_off:int -> t -> ?dst_off:int -> int -> unit
+end
+
+module type S = sig
+  type buf
+  type nonrec t = buf t
+
+  include R with type buf := buf and type t := t
+  include W with type t := t
 end
